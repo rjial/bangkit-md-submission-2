6de52cbaf3a67c940ad3800aaaf5f39f7818980e
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-09-04 00:29:36 +0700

    add searchbar on MainActivity

--- app/src/main/java/com/rjial/githubprofile/MainActivity.kt
+++ app/src/main/java/com/rjial/githubprofile/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rjial.githubprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.rjial.githubprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchView.setupWithSearchBar(binding.searchBar)
+        binding.searchView
+            .editText
+            .setOnEditorActionListener { v, actionId, event ->
+                binding.searchBar.text = binding.searchView.text
+                binding.searchView.hide()
+                Toast.makeText(this@MainActivity, v.text, Toast.LENGTH_SHORT).show()
+
+                false
+            }
     }
 }
