58b2d9501752d18dccb5b5f12a833b2f711939f8
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-09-04 09:14:05 +0700

    add DetailUsernameResponse, UsernameFollowingResponse, and updated search function

--- app/src/main/java/com/rjial/githubprofile/service/SearchAPIInterface.kt
+++ app/src/main/java/com/rjial/githubprofile/service/SearchAPIInterface.kt
@@ -1,11 +1,21 @@
 package com.rjial.githubprofile.service
 
+import com.rjial.githubprofile.model.response.DetailUsernameResponse
 import com.rjial.githubprofile.model.response.SearchGithubResponse
+import com.rjial.githubprofile.model.response.UsernameFollowingResponse
+import com.rjial.githubprofile.model.response.UsernameFollowingResponseItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SearchAPIInterface {
     @GET("search/users")
     fun getSearchResult(@Query("q") keyword: String): Call<SearchGithubResponse>
+
+    @GET("users/{username}/following")
+    fun getUsernameFollowing(@Path("username") username: String): Call<List<UsernameFollowingResponseItem>>
+
+    @GET("users/{username}")
+    fun getDetailUsername(@Path("username") username: String): Call<DetailUsernameResponse>
 }
--- app/src/main/java/com/rjial/githubprofile/ui/adapter/SearchGithubAdapter.kt
+++ app/src/main/java/com/rjial/githubprofile/ui/adapter/SearchGithubAdapter.kt
@@ -1,17 +1,48 @@
 package com.rjial.githubprofile.ui.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.rjial.githubprofile.databinding.ItemProfilesBinding
+import com.rjial.githubprofile.model.response.DetailUsernameResponse
 import com.rjial.githubprofile.model.response.ItemsItem
 import com.rjial.githubprofile.model.response.SearchGithubResponse
+import com.rjial.githubprofile.model.response.UsernameFollowingResponse
+import com.rjial.githubprofile.model.response.UsernameFollowingResponseItem
+import com.rjial.githubprofile.service.ApiService
+import com.rjial.githubprofile.service.SearchAPIInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchGithubAdapter(val listProfile: List<ItemsItem>): RecyclerView.Adapter<SearchGithubAdapter.ViewHolder>() {
+
     class ViewHolder(val binding: ItemProfilesBinding): RecyclerView.ViewHolder(binding.root) {
+        private val apiService = ApiService.getService<SearchAPIInterface>()
         fun applyProfile(result: ItemsItem) {
-            binding.tvNameProfile.text = result.login
+            apiService.getDetailUsername(result.login)
+                .enqueue(object: Callback<DetailUsernameResponse> {
+                    override fun onResponse(
+                        call: Call<DetailUsernameResponse>,
+                        response: Response<DetailUsernameResponse>
+                    ) {
+                        val body = response.body()
+                        if (response.isSuccessful && body != null) {
+                            binding.tvNameProfile.text = if (body.name != null) "${body.name} (${body.login})" else "${body.login}"
+                            binding.tvUsernameProfile.text = "${body.publicRepos} Public Repos - ${body.followers} Followers - ${body.following} Following"
+                        } else {
+                            binding.tvNameProfile.text = result.login
+                            binding.tvUsernameProfile.text = "Failed fetch detail"
+                        }
+                    }
+
+                    override fun onFailure(call: Call<DetailUsernameResponse>, t: Throwable) {
+                        binding.tvUsernameProfile.text = "Failed fetch detail"
+                    }
+
+                })
             Glide.with(binding.root.context)
                 .load(result.avatarUrl)
                 .into(binding.imgPhotoProfile)
